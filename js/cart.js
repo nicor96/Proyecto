4e051dc47b16540ff133d5ab3b8dbdaa916d4f22
nicor96 <nicotele96@gmail.com>
2020-10-25 23:47:18 -0300

    entrega 6

--- js/cart.js
+++ js/cart.js
@@ -64,47 +64,50 @@ function cartTotal () {
         let sendCost = parseInt(document.getElementById("shoppingCartSendCost").innerHTML.replace("$", ""));
         let total = subTotal + sendCost
         document.getElementById("cartTotal").innerHTML = "$" + total
+        var botonCheto = document.getElementById("bottonTypeOfPayment");
+        console.log(botonCheto);
+        botonCheto.style.display = "block";
     }
 }
 
 function envioPremium(){
     tasaDeEnvio = 0.15
     cartSendCost()
-  }
+}
 
-  function envioExpress(){
+function envioExpress() {
     tasaDeEnvio = 0.07
     cartSendCost()
-  }
+}
 
-  function envioStandard(){
+function envioStandard() {
     tasaDeEnvio = 0.05
     cartSendCost()
-  }    
+}
 
 
 function productSubTotal(quantity, subT, cartProduct) {
-        
+
     price = cartProduct.unitCost;
-     
+
     if (cartProduct.currency === "USD") {
-           subTotalProducto = quantity * price * 40;
-        } 
+        subTotalProducto = quantity * price * 40;
+    }
     else {
-            subTotalProducto = quantity * price;
-        }
-        
-    document.getElementById(subT).innerHTML = "$"+ subTotalProducto;
+        subTotalProducto = quantity * price;
+    }
+
+    document.getElementById(subT).innerHTML = "$" + subTotalProducto;
     cartSubTotal();
     cartSendCost();
 }
-    
-function showCart(array){
+
+function showCart(array) {
 
     let htmlContentToAppend = "";
-        for(let i = 0; i < array.length; i++) {
-            let cartProduct = array[i];
-            htmlContentToAppend += `
+    for (let i = 0; i < array.length; i++) {
+        let cartProduct = array[i];
+        htmlContentToAppend += `
                <div class="list-group-item">
 
                     <div class="row">
@@ -129,34 +132,105 @@ function showCart(array){
                     </div>
 
                 </div>`
-        }
-        document.getElementById("cart-display").innerHTML = htmlContentToAppend;
-            
-        for (let i = 0; i < array.length; i++) {         
-            let cartProduct = array[i];
-            productSubTotal(cartProduct.count, "productSubTotal" + i, cartProduct);
-       }
+    }
+    document.getElementById("cart-display").innerHTML = htmlContentToAppend;
 
-        let quantity = document.getElementsByClassName("quantity");
-
-        for (let i = 0; i < quantity.length; i++) {              
-            let cartItem = array[i];
-            let coso = quantity[i];
-
-            coso.onchange = function (e) {
-                productSubTotal(e.target.value, "productSubTotal" + i, cartItem)
-            }
-        }
+    for (let i = 0; i < array.length; i++) {
+        let cartProduct = array[i];
+        productSubTotal(cartProduct.count, "productSubTotal" + i, cartProduct);
     }
 
-document.addEventListener("DOMContentLoaded", function(e){
-        getJSONData(CART_INFO_URL_DESAFIATE).then(function(resultObj){
-            if (resultObj.status === "ok"){
+    let quantity = document.getElementsByClassName("quantity");
+
+    for (let i = 0; i < quantity.length; i++) {
+        let cartItem = array[i];
+        let coso = quantity[i];
+
+        coso.onchange = function (e) {
+            productSubTotal(e.target.value, "productSubTotal" + i, cartItem)
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(CART_INFO_URL_DESAFIATE).then(function (resultObj) {
+        if (resultObj.status === "ok") {
             currentCartProductsArray = resultObj.data;
             showCart(currentCartProductsArray.articles);
             cartSubTotal();
             cartSendCost();
         }
-    
+
     })
 });
+
+  
+  //Info en el modal
+
+
+function showPaymentMethods() {
+    let content = "";
+    let payments = document.getElementsByName("pago");
+
+    for (var i = 0; i < payments.length; i++) {
+        if (payments[i].checked) {
+            if (payments[i].value == "credit") {
+                content = `
+                <h5>Nombre del titular</h5>
+                <div class="row">
+                    <div class="col-8">
+                        <input type="text" id="validationCustom01" class="form-control" required>
+                    </div>
+                    <div class="col"></div>
+                </div><br>
+                <h5>Número de tarjeta</h5>
+                <div class="row">
+                  <input type="text" id="validationCustom02" class="form-control col-5 ml-3" required> – 
+                  <input type="text" id="validationCustom03" class="form-control col-2" required>
+                  <div class="col"></div>
+				</div><br>
+                <br>
+                <h5>Cuotas a pagar</h5>
+				<div class="row">
+                    <div class="col-5">
+                     	<select name="cuotas" class="form-control" required>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>6</option>
+                        <option>12</option>
+                     	</select>
+                    </div>
+                    <div class="col"></div>
+                </div><br>
+              </div>`;
+
+            } else {
+                content = `
+                <h5>Nombre</h5>
+                <h7>Carlos Bueno</h7>
+                <br>
+                <br>
+                <h5>Banco</h5>
+                <div class="row">
+                    <div class="col-5">
+                    	<select name="bancos" id="validationCustom01" class="form-control" required>
+                        <option onselect="console.log("asdad")">BBVA</option>
+                        <option>BROU</option>
+                        <option>HSBC</option>
+                        <option>Itaú</option>
+                        <option>Santander</option>
+                    	</select>   
+                    </div>
+                    <div class="col"></div>
+                </div><br>
+                <h5>Número de cuenta</h5>
+                <div>
+                    <h7>1456987</h7>
+                </div>
+                <br>`
+            }
+        }
+    }
+
+    document.getElementById("paymentMethods").innerHTML = content;
+}
